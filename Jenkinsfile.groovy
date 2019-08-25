fbecffbbd00d5ed5a5ce64cdefc0c6441c2453cc
farrukh90 <farrukhsadykov@gmail.com>
2019-08-25 16:08:03 -0500

    added new lines

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -58,11 +58,6 @@ pipeline{
                 }
             }
         }
-        stage("Pull Repo"){
-            steps{
-                git("https://github.com/farrukh90/packerde.git")
-            }
-        }
         stage("Build Image"){
             steps{
                 //sh "packer build updated/updated.json"
