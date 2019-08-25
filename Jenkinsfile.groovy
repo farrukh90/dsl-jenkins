ad87089af5374b1b571ab89f538d36a5b8c31fbf
farrukh90 <farrukhsadykov@gmail.com>
2019-08-25 15:28:14 -0500

    asdf

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,7 +34,8 @@ pipeline{
         stage("Write to a file"){
             steps{
                 ws("tmp/"){
-                    writeFile text: "Test", file: "TestFile"
+                    writeFile text: "Test"\nline2, file: "TestFile"
+                    sh "cat TestFile"
                 }
             }
         }
