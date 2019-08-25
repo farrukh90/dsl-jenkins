605754643efec8d273ede30ad5d54eaba86c2ebc
farrukh90 <farrukhsadykov@gmail.com>
2019-08-25 15:39:38 -0500

    added  ls

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -61,6 +61,7 @@ pipeline{
         stage("Pull Repo"){
             steps{
                 git("https://github.com/farrukh90/packerde.git")
+                sh "ls"
             }
         }
     }
